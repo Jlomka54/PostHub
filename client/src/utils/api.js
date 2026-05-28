8c0086a9584769fd7f88701af444012b06dc3110
Bohdan Haponiuk <jlomka54@gmail.com>
2026-05-28 09:37:18 +0400

    vercel file for deploy

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,3 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3002/api";
+export const API_BASE_URL = process.env.REACT_APP_API_URL || "/api";
 
 export const API_ORIGIN = API_BASE_URL.replace(/\/api\/?$/, "");
